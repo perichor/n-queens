2899e148d80e188f8cbaad7cdc88ec7bc7955936
perichor <sandejeremy@gmail.com>
2016-12-22 11:30:11 -0800

    Finished N Queens Solvers

--- src/solvers.js
+++ src/solvers.js
@@ -77,12 +77,16 @@ window.findNQueensSolution = function(n) {
         solution.attributes[i][j] = 1;
         queenCount++;
         
-        if (solution.hasAnyRooksConflicts() === true) {
+        if (solution.hasAnyQueensConflicts() === true) {
           solution.attributes[i][j] = 0;
           queenCount--;
         } else {
           if (queenCount !== n) {
-            recursor(i + 1);  
+            recursor(i + 1);
+            if (!solutionFound) {
+              solution.attributes[i][j] = 0;
+              queenCount--;
+            }
           } else {
             solutionFound = true;
           }
@@ -93,14 +97,48 @@ window.findNQueensSolution = function(n) {
     });
   };
   recursor(0);
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution.rows();
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var solutionFound = false;
+  var solution = new Board(window.makeEmptyMatrix(n));
+  var queenCount = 0;
+  if (n === 0) {
+    return 1;
+  }
+  var recursor = function(i) {
+    _.each(solution.rows()[i], function(element, j) {
+      if (!solutionFound) {
+        solution.attributes[i][j] = 1;
+        queenCount++;
+        if (solution.hasAnyQueensConflicts() === true) {
+          solution.attributes[i][j] = 0;
+          queenCount--;
+        } else {
+          if (queenCount !== n) {
+            recursor(i + 1);
+            solutionFound = false;
+            solution.attributes[i][j] = 0;
+            queenCount--;
+          } else {
+            solutionFound = true;
+            solutionCount++;
+            solution.attributes[i][j] = 0;
+            queenCount--;
+          }
+        }
+      }
+    });
+  };
+  recursor(0);
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
+
+
+
